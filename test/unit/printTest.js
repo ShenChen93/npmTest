4ac745fbcf9c15eb2e857a7681d1899702418c1b
Shen Chen <shench@amazon.com>
2019-07-25 14:37:18 -0700

    Travis fail test

--- test/unit/printTest.js
+++ test/unit/printTest.js
@@ -3,7 +3,7 @@ const print = require('@src/print');
 
 describe('Print test', () => {
     it('| test print', (done) => {
-        expect(print('a')).equal('a');
+        expect(print('a')).equal('Ron');
         done();
     });
 });
